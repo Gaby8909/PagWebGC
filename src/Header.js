9ae20e6eba8c27d6223057ca00120de3ae7b71e5
Gaby8909 <gabi_oa@hotmail.com>
2021-09-05 00:12:09 +0200

    creando estilos

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="header">
+          <div className="header__menu">
+
+              <a href=""><img src=""/></a>
+              <div className="header__nav">
+                  <a href="">Acerca de mi</a>
+                  <a href="">Proyectos</a>
+                  <a href="">Contacto</a>
+
+              </div>
+          </div>
+        </div>
+    )
+}
+
+
+
+
+export default Header
